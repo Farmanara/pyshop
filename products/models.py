fa9ff3ecdb258a1ba45e76e32390074110a72977
farmanara <farmanara.code@gmail.com>
2022-01-08 21:25:20 +0330

    returning products names added

--- products/models.py
+++ products/models.py
@@ -8,8 +8,14 @@ class Product(models.Model):
     stock=models.IntegerField()
     image_url=models.CharField(max_length=2083)
 
+    def __str__(self):
+        return self.name
+
 class Offer(models.Model):
     ''' discounts model '''
     code=models.CharField(max_length=7)
     description=models.CharField(max_length=100)
     discount=models.FloatField()
+
+    def __str__(self):
+        return "Coupon"
